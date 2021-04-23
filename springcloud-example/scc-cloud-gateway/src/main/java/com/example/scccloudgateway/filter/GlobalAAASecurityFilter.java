05ebd3db92dda33aa4d1c79c15cd9cd1902fe917
fenghd <fenghd@mob.com>
2021-04-23 19:00:42 +0800

    add ignore

--- springcloud-example/scc-cloud-gateway/src/main/java/com/example/scccloudgateway/filter/GlobalAAASecurityFilter.java
+++ springcloud-example/scc-cloud-gateway/src/main/java/com/example/scccloudgateway/filter/GlobalAAASecurityFilter.java
@@ -17,7 +17,6 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
